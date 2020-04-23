bee7fed50a0181a9672736631162ce75565e115b
yu1441 <yu1441@qq.com>
2020-04-23 13:47:17 +0800

    环境升级

--- app/src/main/java/com/yujing/chuankou/App.java
+++ app/src/main/java/com/yujing/chuankou/App.java
@@ -1,8 +1,6 @@
 package com.yujing.chuankou;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
-import android.content.Context;
 import android.util.Log;
 
 import com.yujing.ycrash.YCrash;
