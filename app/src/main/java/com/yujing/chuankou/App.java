c317306b671fa889c2208946bdbac9756e351d3f
余静 <yu1441@qq.com>
2021-02-04 16:26:30 +0800

    更新demo支持kotlin

--- app/src/main/java/com/yujing/chuankou/App.java
+++ app/src/main/java/com/yujing/chuankou/App.java
@@ -4,7 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.hn.utils.HnUtils;
-import com.yujing.utils.YApp;
+import com.yujing.utils.YLog;
+import com.yujing.utils.YPath;
 import com.yujing.ycrash.YCrash;
 
 
@@ -12,7 +13,7 @@ public class App extends Application {
 
     public static App INSTANCE;
 
-    public static App getInstance() {
+    public static App get() {
         if (INSTANCE == null) {
             synchronized (App.class) {
                 if (INSTANCE == null) {
@@ -22,13 +23,21 @@ public class App extends Application {
         }
         return INSTANCE;
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        INSTANCE=this;
-        HnUtils.Companion.init(this);
+        INSTANCE = this;
+        //异常捕获
         YCrash.getInstance().init(this);
         YCrash.getInstance().setAppName(getString(R.string.app_name));
-        YCrash.getInstance().setCrashInfoListener(appInfo -> Log.e("崩溃拦截",appInfo.崩溃信息));
+        YCrash.getInstance().setCrashInfoListener(appInfo -> Log.e("崩溃拦截", appInfo.崩溃信息));
+        //作者的测试
+        HnUtils.Companion.init(this);
+        HnUtils.Companion.initDevice();
+        //打开日志保存
+        YLog.saveOpen(YPath.getFilePath(this, "log"));
+        //保存最近30天日志
+        YLog.delDaysAgo(30);
     }
 }
